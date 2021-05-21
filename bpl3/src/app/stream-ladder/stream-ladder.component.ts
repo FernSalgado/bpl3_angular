f37797d420779f6f840d946814ed908d04cfdf49
Fernando Salgado <f.da.silva.salgado@accenture.com>
2021-05-21 19:53:35 -0300

    StreamerLadder

--- bpl3/src/app/stream-ladder/stream-ladder.component.ts
+++ bpl3/src/app/stream-ladder/stream-ladder.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Team } from '../models/Team';
+import { ApiService } from '../service/api.service';
 
 @Component({
   selector: 'app-stream-ladder',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stream-ladder.component.css']
 })
 export class StreamLadderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private apiService:ApiService ) { }
+  dataSource2 :MatTableDataSource<Team>;
+  teams:Team[] = [];
+  displayedColumns2: string[] = ['name', 'totalPoints'];
+  
+  async ngOnInit(){
+    setInterval(this.loadTeams,20000);
+    await this.loadTeams();
+    this.dataSource2 = new MatTableDataSource(this.teams);
   }
+
+  async loadTeams(){
+    this.teams = await this.apiService.getTeams();
+  }
+
+  checkTeam(teamName){
+    return "../../assets/"+ teamName+".png";
+}
 
 }
